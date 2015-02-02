77d83b5cc096bc991d52b1f4b31d5a1f39b03380
Stephan Bönnemann <stephan@boennemann.me>
2015-02-02 20:46:49 +0100

    feat: initial module

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var exports = module.exports = function (input) {
+  var errorHandler = exports.throwError
+  if (typeof errorHandler === 'function') errorHandler = input
+
+  return function(handler) {
+    return function(err) {
+      if (err) if (!errorHandler.apply(null, arguments)) return
+
+      handler.apply(this, Array.prototype.slice.call(arguments, 1))
+    }
+  }
+}
+
+exports.throwError = function (err) {
+  throw err
+}
